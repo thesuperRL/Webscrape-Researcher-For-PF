9d8ff9920798c07cdbcc4754eeaa849a27188152
thesuperRL <89622221+thesuperRL@users.noreply.github.com>
2023-08-04 17:10:10 -0700

    Added more clarity and link ranks

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 from scripts import ai_relation_checker
 
-def run_web_query(query_string, googlescholarsearch = False, first_responses = 10):
+def run_web_query(query_string, googlescholarsearch = False, total_searches = 10, first_responses = 10):
     print(" ------------------------------------------ ")
     print("             STARTING FUNCTION              ")
     print(" ------------------------------------------ ")
-    query_data, text_list = ai_relation_checker.construct_query_json(query_string, first_responses, googlescholarsearch)
+    query_data, text_list = ai_relation_checker.construct_query_json(query_string, total_searches, googlescholarsearch)
     response_data = ai_relation_checker.query(query_data)
     sorted_query = ai_relation_checker.match_and_sort(response_data, text_list)
     ai_relation_checker.print_best_match(sorted_query, first_responses)
-    
-run_web_query("Legalizing Marijuana can cause its use to be increased")
--- scripts/ai_relation_checker.py
+++ scripts/ai_relation_checker.py
@@ -49,9 +49,11 @@ def match_and_sort(response_data, text_list):
     print(" ------------------------------------------ ")
     print("              SORTING RESULTS               ")
     print(" ------------------------------------------ ")
-    print("    These two numbers should match:")
-    print("    " + str(len(response_data)))
-    print("    " + str(len(text_list)))
+    if (len(response_data) == 2):
+        print(response_data)
+    else:
+        print("    All Good")
+        print("    " + str(len(text_list)) + " Different Paragraphs/Segments")
     for response in range(0, len(response_data)):
         text_list[response]["relevance"] = response_data[response]
         
@@ -59,16 +61,38 @@ def match_and_sort(response_data, text_list):
     text_list.sort(key=sorting_function_via_relevance, reverse=True)
     return text_list
 
+def sorting_function_via_score(array):
+     return array[1]
+ 
 def print_best_match(sorted_query, first_responses):
     print(" ------------------------------------------ ")
     print("          PRINTING BEST RESPONSES           ")
     print(" ------------------------------------------ ")
-    link_set = set([])
-    for item in range(0, first_responses):
-        dumped = json.dumps(sorted_query[item])
-        link_set.add(sorted_query[item]['url'])
-        print(dumped)
-    print(link_set)
+    link_dict = {}
+    for item in range(0, len(sorted_query)):
+        if item < first_responses:
+            dumped = json.dumps(sorted_query[item])
+            print(dumped)
+        
+        if sorted_query[item]['url'] not in link_dict:
+            link_dict[ sorted_query[item]['url'] ] = [len(sorted_query) - item, 1]
+        else:
+            link_dict[ sorted_query[item]['url'] ] = [
+                link_dict[ sorted_query[item]['url'] ][0] + len(sorted_query) - item, 
+                link_dict[ sorted_query[item]['url'] ][1] + 1
+                ]
+            
+    scored_links = []
+    for link_string in link_dict.keys():
+        score = link_dict[link_string][0] / link_dict[link_string][1]
+        scored_links.append([link_string, score])
+    
+    print()
+    print()
+    print(" ------------ LINKS RANKING ------------ ")
+    scored_links.sort(key=sorting_function_via_score, reverse=True)
+    for item in range(0, min(len(scored_links), first_responses)):
+        print(scored_links[item][0] + ": " + str(scored_links[item][1]))
     
 def run_web_query(query_string, first_responses):
     print(" ------------------------------------------ ")
